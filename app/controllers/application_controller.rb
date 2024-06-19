e0934d6a436cef093974bccbc64b4d118f1bccff
josejojis <josejojis@gmail.com>
2024-06-19 18:36:39 +0800

    new by chatgpt commit 5 new month devise production 1.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  rescue_from CanCan::AccessDenied do |exception|
+    # Log the access denied event for debugging purposes
+    Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect} for user #{current_user.inspect}"
+
+    # Set the flash message and redirect the user
+    flash[:alert] = "You are not authorized to access this page."
+    redirect_to root_path # Or another appropriate path
   end
-  
+end 
