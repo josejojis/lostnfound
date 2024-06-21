7b2f34733d304efe48dcfef4e0e31e7fbbcfb8ff
josejojis <josejojis@gmail.com>
2024-06-22 01:00:43 +0800

    new by roles changed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,8 @@ class Ability
   def initialize(user)
     user ||= User.new # Guest user
 
-    if user.persisted? && user.admin_role?
+    if user.admin?
       can :manage, :all
-      can :access, :rails_admin  # only admin can access Rails Admin
-      can :manage, :dashboard    # allow access to dashboard
-    elsif user.persisted? && user.agent_role?
-      can :create, FoundReport
-      can :update, FoundReport, user_id: user.id
-      can :destroy, FoundReport, user_id: user.id
     else
       can :read, :all
       can :create, FoundReport
